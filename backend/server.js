052782a2878c7e4c38352b829c8bd23e26653d14
Rahul Kumar <142046793+N-rahul-35@users.noreply.github.com>
2025-08-24 21:40:42 +0530

    add vercel app link in backend

--- backend/server.js
+++ backend/server.js
@@ -231,7 +231,10 @@ app.use(limiter);
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin:
+      process.env.CORS_ORIGIN ||
+      "http://localhost:3000" ||
+      "https://mini-plant-store.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
